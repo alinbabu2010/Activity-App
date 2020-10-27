e4d4862c2b286e8642b706dc73c68fc3ddf6075a
Alin Babu <alin@qburst.com>
2020-10-27 18:13:33 +0530

    Adding passing values between activities

--- app/src/main/java/com/example/activity/MainActivity.kt
+++ app/src/main/java/com/example/activity/MainActivity.kt
@@ -1,28 +1,35 @@
 package com.example.activity
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+
 
 /**
  * This the main activity of the app
  */
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Getting text from input field.
         val textId = findViewById<EditText>(R.id.inputField1)
-        val inputText = textId.text.toString()
+        var inputText = textId.text.toString()
+        Log.i("MainActivity", inputText)
 
         // Getting button by id and move to next activity on button click
         val buttonid = findViewById<Button>(R.id.button1)
         buttonid.setOnClickListener {
-            val next = Intent(this@MainActivity,SubActivity::class.java)
-            startActivity(next)
+            val intent = Intent(this@MainActivity, SubActivity::class.java)
+            intent.putExtra("input", inputText)
+            startActivity(intent)
+
         }
     }
 }
--- app/src/main/java/com/example/activity/SubActivity.kt
+++ app/src/main/java/com/example/activity/SubActivity.kt
@@ -1,9 +1,11 @@
 package com.example.activity
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  * This is the second activity to be used in the app.
@@ -12,6 +14,12 @@ class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+
+        // Accesing textview and assig value from MainActivity to textview
+        val message = intent.getStringExtra("input").toString()
+        Log.i("SubActivity",message)
+        val text1 = findViewById<TextView>(R.id.textView2)
+        text1.setText(message)
 
         // Getting button by id and move to previous activity on button click
         val buttonid = findViewById<Button>(R.id.button2)
